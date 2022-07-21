2d28a6575424f0c2c5ba046d532a55b62ec9c648
altamir95 <altamir95@yandex.ru>
2022-07-21 08:32:53 +0300

    Initial commit

---
+++ RuntimeInFramework/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuntimeInFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PythonOperation pop = new PythonOperation();
+            pop.Initialize();
+            pop.Execute();
+            Console.ReadLine();
+        }
+    }
+}
